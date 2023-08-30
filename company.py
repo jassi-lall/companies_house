a2cf97c21fcf7bcc05acd14fe50b0663017b5955
Jaspreet Lall <67714004+jassi-lall@users.noreply.github.com>
2023-08-30 09:29:22 +0100

    Add files via upload

--- company.py
+++ company.py
@@ -3,11 +3,11 @@ Search officers in companies house.
 
 ! IMPORTANT !
 Use the script responsibly as this pings a government website.
-- Use the official API instead if calling on the script multiple times.
+- Use the official API instead - if calling on the script multiple times.
 - Do not put undue stress on their servers. Minimise data extraction and use time delays.
 - Check their robots.txt file . Suggested crawl delay 10.
 
-Given a search term (e.g. last name) it will aggregate all the data into a clean tabular format that can be used for further analysis
+Given a search term (e.g. last name) it will aggregate all the data into a tabular format that can be used for further analysis
 
 Example use case:
 E.g. Find all officers that have last name "Smith" from Swansea.
